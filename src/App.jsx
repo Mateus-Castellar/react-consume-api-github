4d52245f5a843876695a2d7031bed8699212da6d
Mateus Castellar <mateuscastellar@outlook.com>
2023-01-24 16:46:29 -0300

    feat: Primeiro componente em react

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./styles/global.scss";
+import { RepositoryList } from "./components/RepositoryList";
 
 export function App() {
-  return <h1>Base do react</h1>;
+  return <RepositoryList />;
 }
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,39 @@
+const repositoryName = "unform";
+
+export function RepositoryList() {
+  return (
+    <section className="repositoty-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>react js github api</p>
+
+          <a href="">acessar repositório</a>
+        </li>
+
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>react js github api</p>
+
+          <a href="">acessar repositório</a>
+        </li>
+
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>react js github api</p>
+
+          <a href="">acessar repositório</a>
+        </li>
+
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>react js github api</p>
+
+          <a href="">acessar repositório</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
